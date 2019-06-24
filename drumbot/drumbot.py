c9ec25764fdd39ee5f5cc74dcec7b859989fcee9
S Stewart <iamtheworst@programmer.net>
2019-06-24 18:50:43 -0500

    Add basic API wrapper in Python

---
+++ drumbot/drumbot.py
@@ -0,0 +1,22 @@
+"""
+Asyncio Drumbot API implementation for Python
+"""
+
+import aiohttp
+
+
+class Drumbot:
+    def __init__(self,
+                 session=aiohttp.ClientSession()):
+        self.session = session
+        self.patterns = 'https://api.noopschallenge.com/drumbot/patterns'
+
+    async def get_all_patterns(self):
+        async with self.session.get(self.patterns) as p:
+            return p.json()
+    
+    async def get_pattern(self, pattern):
+        def make_url(pattern):
+            return f'https://api.noopschallenge.com/drumbot/patterns/{pattern}'
+        async with self.session.get(make_url(pattern)) as p:
+            return p.json()
---
+++ drumbot/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name="drumbot",
+    version="1.0",
+    author="tilda",
+    author_email="tda@lmao.tf",
+    description=("Drumbot API wrapper for Python."),
+    license="MIT",
+    keywords="drumbot github noopschallenge",
+    url="https://github.com/utilefordiscord/drumbot-implementation",
+    packages=['drumbot'],
+    long_description="Drumbot API wrapper for Python.",
+    install_requires=['aiohttp'],
+    classifiers=[
+        "Development Status :: 5 - Release",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
